95d67f5c9dbfec1e5089ca07a9835cf79ffad9a4
Shivam Sharma <shivamsharma2577@gmail.com>
2024-03-01 15:25:49 +0530

    Type converters added for our database

--- app/src/main/java/com/example/newsapp/db/ArticleDatabase.kt
+++ app/src/main/java/com/example/newsapp/db/ArticleDatabase.kt
@@ -4,13 +4,16 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.example.newsapp.Article
+import androidx.room.TypeConverters
+import com.example.newsapp.models.Article
 
 
 @Database(
     entities = [Article::class],
     version = 1
 )
+
+@TypeConverters(Converters::class)
 abstract class ArticleDatabase: RoomDatabase() {
 
     abstract fun getArticleDao(): ArticleDao
